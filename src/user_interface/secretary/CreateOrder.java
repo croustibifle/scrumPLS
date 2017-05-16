d8f2459e588a5450f30397f3ebad46eeec75f769
ARGONIS <ARGONIS@172.18.24.152>
2017-05-16 15:02:37 +0200

    Interface Order

--- src/user_interface/secretary/CreateOrder.java
+++ src/user_interface/secretary/CreateOrder.java
@@ -4,7 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -15,21 +15,50 @@ public class CreateOrder extends JPanel{
 
 	    this.setLayout(new BorderLayout());
 	    
+	    JLabel title = new JLabel("ORDER CREATION",JLabel.CENTER);
+	    
+	    JPanel entryPanel = new JPanel();
+	    JLabel clientLabel = new JLabel("Client : ");
+	    JComboBox clientCB = new JComboBox();
+	    JButton addClientButton = new JButton("+");
+	    JLabel categoryLabel = new JLabel("Category : ");
+	    JComboBox categoryCB = new JComboBox();
+	    JButton addCategoryButton = new JButton("+");
+	    JLabel speciesLabel = new JLabel("Species : ");
+	    JComboBox speciesCB = new JComboBox();
+	    JButton addSpeciesButton = new JButton("+");
+	    JLabel analysisLabel = new JLabel("Analysis : ");
+	    JComboBox analysisCB = new JComboBox();
+	    JLabel sampleLabel = new JLabel("Number of samples : ");
+	    JTextField sampleTF = new JTextField();	
+	    JLabel voidLabel = new JLabel();
+	    
+	    entryPanel.setLayout(new GridLayout(5,3));
+	    entryPanel.add(clientLabel);
+	    entryPanel.add(clientCB);
+	    entryPanel.add(addClientButton);
+	    entryPanel.add(categoryLabel);
+	    entryPanel.add(categoryCB);
+	    entryPanel.add(addCategoryButton);
+	    entryPanel.add(speciesLabel);
+	    entryPanel.add(speciesCB);
+	    entryPanel.add(addSpeciesButton);
+	    entryPanel.add(analysisLabel);
+	    entryPanel.add(analysisCB);
+	    entryPanel.add(voidLabel);
+	    entryPanel.add(sampleLabel);
+	    entryPanel.add(sampleTF);
+	    
 	    JPanel buttonPanel = new JPanel();
-	    JPanel mySecondPanel = new JPanel();
-	    JButton myFirstButton = new JButton("Create");
-	    JButton mySecondButton = new JButton("Cancel");
-	    JLabel myLabel = new JLabel("Category : ");
-	    JTextField myJTF = new JTextField();
+	    JButton createButton = new JButton("Create");
+	    JButton cancelButton = new JButton("Cancel");
 	    
 	    buttonPanel.setLayout(new GridLayout(1,2));
-	    buttonPanel.add(myFirstButton);
-	    buttonPanel.add(mySecondButton);
+	    buttonPanel.add(createButton);
+	    buttonPanel.add(cancelButton);
 	    
-	    mySecondPanel.add(myLabel);
-	    mySecondPanel.add(myJTF);
-	    
-	    this.add(buttonPanel, BorderLayout.CENTER);
-	    this.add(mySecondPanel, BorderLayout.NORTH);
+	    this.add(title, BorderLayout.NORTH);
+	    this.add(entryPanel, BorderLayout.CENTER);
+	    this.add(buttonPanel, BorderLayout.SOUTH);	    
 	}
 }
