c14d6cfb94a86c59d15252ba550a301aa0120952
Cyp2C19 <cyprien8@hotmail.fr>
2017-05-16 15:58:49 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import javax.swing.JFrame;
 
+import user_interface.MainFrame;
+import user_interface.home.Home;
 import user_interface.secretary.CreateSpecies;
 
 public class Main {
 
 	public static void main(String[] args) {
 		//new MainFrame();
-		JFrame j = new JFrame();
+		MainFrame j = new MainFrame();
+		Home h = new Home();
 		CreateSpecies c = new CreateSpecies();
-		j.add(c);
-		j.setVisible(true);
-		
+		j.add(h);
 	}
 }
