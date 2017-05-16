627db3691dad474917f5c78351ebe2f47e770d96
bibiche01 <hugoboss01300@hotmail.fr>
2017-05-16 16:25:41 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import javax.swing.JFrame;
-
 import user_interface.MainFrame;
 import user_interface.home.Home;
 import user_interface.secretary.CreateCategory;
@@ -9,7 +7,7 @@ import user_interface.validator.ValidatorMenu;
 public class Main {
 
 	public static void main(String[] args) {
-		//new MainFrame();
+		new MainFrame();
 		MainFrame j = new MainFrame();
 		Home h = new Home();
 		CreateSpecies c = new CreateSpecies();
