c3d011aececf498d7b4d9b6807143e4931be1739
Cyp2C19 <cyprien8@hotmail.fr>
2017-05-16 11:31:54 +0200

    Update SecretaryMenu.java

--- src/SecretaryMenu.java
+++ src/SecretaryMenu.java
@@ -1,26 +1,25 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class SecretaryMenu {
+public class SecretaryMenu extends JFrame {
 
-	public static void main ()
-    {
-	 JFrame myFrame = new JFrame("Secretary Menu");
-     myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     
-     myFrame.setLayout(new BorderLayout());
-     
-     JPanel myPanel = new JPanel();
-     myPanel.setLayout(new GridLayout(2,2));
-
-     myPanel.add(new JButton("Order"));
-     myPanel.add(new JButton("Species"));
-     myPanel.add(new JButton("Customer"));
-     myPanel.add(new JButton("Category"));
-
-     myFrame.add(myPanel, BorderLayout.CENTER);
-
-     myFrame.pack();
-     myFrame.setVisible(true);
+	public SecretaryMenu(){
+		 this.setTitle("Secretary Menu");
+	     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	     this.setLayout(new BorderLayout());
+	     
+	     JPanel myPanel = new JPanel();
+	     myPanel.setLayout(new GridLayout(2,2));
+	
+	     myPanel.add(new JButton("Order"));
+	     myPanel.add(new JButton("Species"));
+	     myPanel.add(new JButton("Customer"));
+	     myPanel.add(new JButton("Category"));
+	
+	     this.add(myPanel, BorderLayout.CENTER);
+	
+	     this.pack();
+	     this.setVisible(true);
     }
 }
+
