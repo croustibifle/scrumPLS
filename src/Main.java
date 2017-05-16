b543ec790fd1a96a84f1b79d903645b29208263f
Cyp2C19 <cyprien8@hotmail.fr>
2017-05-16 17:02:26 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import user_interface.MainFrame;
+import javax.swing.JFrame;
+
 import user_interface.home.Home;
 import user_interface.secretary.CreateCategory;
 import user_interface.secretary.CreateSpecies;
@@ -8,8 +9,10 @@ import user_interface.validator.ValidatorMenu;
 public class Main {
 
 	public static void main(String[] args) {
-		new MainFrame();
-		MainFrame j = new MainFrame();
+		JFrame j = new JFrame();
+		j.setTitle("Genindex application");
+	    j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	    j.setSize(800, 600);
 //		Home h = new Home();
 //		CreateSpecies c = new CreateSpecies();
 //		ValidatorMenu m = new ValidatorMenu();
