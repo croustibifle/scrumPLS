9031955d21f38af48f422af1451beec792535831
ARGONIS <ARGONIS@172.18.24.152>
2017-05-16 15:37:40 +0200

    Customer Interface

--- src/user_interface/secretary/CreateCustomer.java
+++ src/user_interface/secretary/CreateCustomer.java
@@ -14,21 +14,30 @@ public class CreateCustomer extends JPanel{
 	public CreateCustomer(){
 	    this.setLayout(new BorderLayout());
 	    
-	    JPanel buttonPanel = new JPanel();
+	    JLabel title = new JLabel("CUSTOMER CREATION",JLabel.CENTER);
+	    
 	    JPanel entryPanel = new JPanel();
+	    JLabel clientLabel = new JLabel("Client : ");
+	    JTextField clientTF = new JTextField();
+	    JLabel cityLabel = new JLabel("City : ");
+	    JTextField cityTF = new JTextField();
+	    
+	    entryPanel.setLayout(new GridLayout(2,2));
+	    entryPanel.add(clientLabel);
+	    entryPanel.add(clientTF);
+	    entryPanel.add(cityLabel);
+	    entryPanel.add(cityTF);
+	    
+	    JPanel buttonPanel = new JPanel();
 	    JButton createButton = new JButton("Create");
 	    JButton cancelButton = new JButton("Cancel");
-	    JLabel categoryLabel = new JLabel("Category : ");
-	    JTextField categoryTF = new JTextField();
 	    
 	    buttonPanel.setLayout(new GridLayout(1,2));
 	    buttonPanel.add(createButton);
 	    buttonPanel.add(cancelButton);
 	    
-	    entryPanel.add(categoryLabel);
-	    entryPanel.add(categoryTF);
-	    
-	    this.add(buttonPanel, BorderLayout.CENTER);
-	    this.add(entryPanel, BorderLayout.NORTH);
+	    this.add(title, BorderLayout.NORTH);
+	    this.add(entryPanel, BorderLayout.CENTER);
+	    this.add(buttonPanel, BorderLayout.SOUTH);
 	}
 }
