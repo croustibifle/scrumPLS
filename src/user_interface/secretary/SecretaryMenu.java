c26130ff4ecf9f94ab2db468176b7d6d4cff0310
bibiche01 <hugoboss01300@hotmail.fr>
2017-05-17 15:26:11 +0200

    secretary menu fini

--- src/user_interface/secretary/SecretaryMenu.java
+++ src/user_interface/secretary/SecretaryMenu.java
@@ -1,30 +1,111 @@
 package user_interface.secretary;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+/*
+ * Created by JFormDesigner on Wed May 17 15:25:15 CEST 2017
+ */
 import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class SecretaryMenu extends JFrame {
 
-	public SecretaryMenu(){
-	     this.setLayout(new BorderLayout());
-	     
-	     JPanel buttonPanel = new JPanel();
-	     JButton orderButton = new JButton("Order");
-	     JButton speciesButton = new JButton("Species");
-	     JButton customerButton = new JButton("Customer");
-	     JButton categoryButton = new JButton("Category");
-	     
-	     buttonPanel.setLayout(new GridLayout(2,2));
-	
-	     buttonPanel.add(orderButton);
-	     buttonPanel.add(speciesButton);
-	     buttonPanel.add(customerButton);
-	     buttonPanel.add(categoryButton);
-	
-	     this.add(buttonPanel, BorderLayout.CENTER);
-    }
+
+/**
+ * @author Hugo Muller
+ */
+public class SecretaryMenu extends JPanel {
+	public SecretaryMenu() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+		// Generated using JFormDesigner Evaluation license - Hugo Muller
+		label1 = new JLabel();
+		panel1 = new JPanel();
+		button1 = new JButton();
+		button2 = new JButton();
+		button3 = new JButton();
+		button4 = new JButton();
+
+		//======== this ========
+		setBackground(new Color(204, 0, 204));
+
+		// JFormDesigner evaluation mark
+		setBorder(new javax.swing.border.CompoundBorder(
+			new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+				"JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+				javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+				java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+		setLayout(new GridBagLayout());
+		((GridBagLayout)getLayout()).columnWidths = new int[] {26, 578, 21, 0};
+		((GridBagLayout)getLayout()).rowHeights = new int[] {107, 276, 53, 0};
+		((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+		((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+
+		//---- label1 ----
+		label1.setText("MENU");
+		label1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		add(label1, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+			GridBagConstraints.CENTER, GridBagConstraints.NONE,
+			new Insets(0, 0, 5, 5), 0, 0));
+
+		//======== panel1 ========
+		{
+			panel1.setBackground(new Color(204, 0, 204));
+			panel1.setLayout(new GridBagLayout());
+			((GridBagLayout)panel1.getLayout()).columnWidths = new int[] {291, 279, 0};
+			((GridBagLayout)panel1.getLayout()).rowHeights = new int[] {137, 132, 0};
+			((GridBagLayout)panel1.getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
+			((GridBagLayout)panel1.getLayout()).rowWeights = new double[] {0.0, 0.0, 1.0E-4};
+
+			//---- button1 ----
+			button1.setText("Order");
+			button1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			panel1.add(button1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 5, 5), 0, 0));
+
+			//---- button2 ----
+			button2.setText("Species");
+			button2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			panel1.add(button2, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 5, 0), 0, 0));
+
+			//---- button3 ----
+			button3.setText("Customer");
+			button3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			panel1.add(button3, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 0, 5), 0, 0));
+
+			//---- button4 ----
+			button4.setText("Category");
+			button4.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			panel1.add(button4, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 0, 0), 0, 0));
+		}
+		add(panel1, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
+			GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+			new Insets(0, 0, 5, 5), 0, 0));
+		// JFormDesigner - End of component initialization  //GEN-END:initComponents
+	}
+
+	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+	// Generated using JFormDesigner Evaluation license - Hugo Muller
+	private JLabel label1;
+	private JPanel panel1;
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+	private JButton button4;
+	// JFormDesigner - End of variables declaration  //GEN-END:variables
 }
-
