6f669a682d1fcd423ce39381e0cf6006e0bc0fb3
bibiche01 <hugoboss01300@hotmail.fr>
2017-05-17 09:17:39 +0200

    menu valida

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 import javax.swing.JFrame;
 
-import user_interface.home.Home;
-import user_interface.secretary.CreateCategory;
-import user_interface.secretary.CreateSpecies;
-import user_interface.validator.ExploreOrder;
 import user_interface.validator.ValidatorMenu;
 
 public class Main {
@@ -15,10 +11,10 @@ public class Main {
 	    j.setSize(800, 600);
 //		Home h = new Home();
 //		CreateSpecies c = new CreateSpecies();
-//		ValidatorMenu m = new ValidatorMenu();
-	    CreateCategory g = new CreateCategory ();
-		ExploreOrder o = new ExploreOrder();
-		j.add(g);
+		ValidatorMenu m = new ValidatorMenu();
+//	    CreateCategory g = new CreateCategory ();
+//		ExploreOrder o = new ExploreOrder();
+		j.add(m);
 		j.setVisible(true);
 	}
 }
--- src/user_interface/validator/ValidatorMenu.java
+++ src/user_interface/validator/ValidatorMenu.java
@@ -1,27 +1,180 @@
 package user_interface.validator;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+/*
+ * Created by JFormDesigner on Tue May 16 17:10:57 CEST 2017
+ */
 
+
+
+/**
+ * @author Hugo Muller
+ */
 public class ValidatorMenu extends JPanel {
-	public ValidatorMenu(){
-		this.setLayout(new BorderLayout());
-		
-		JLabel title = new JLabel("VALIDATOR",JLabel.CENTER);
-		
-		JPanel buttonPanel = new JPanel();
-	    JButton visualizeButton = new JButton("Visualize order");
-	    JButton scrapieButton = new JButton("Create Scrapie test");
-	    
-	    buttonPanel.setLayout(new GridLayout(1,2));
-	    buttonPanel.add(visualizeButton);
-	    buttonPanel.add(scrapieButton);
-	    
-	    this.add(title, BorderLayout.NORTH);
-	    this.add(buttonPanel, BorderLayout.CENTER);
+	public ValidatorMenu() {
+		initComponents();
 	}
+
+	private void button2MouseClicked(MouseEvent e) {
+		// TODO add your code here
+	}
+
+	private void button3MouseClicked(MouseEvent e) {
+		// TODO add your code here
+	}
+
+	private void initComponents() {
+		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+		// Generated using JFormDesigner Evaluation license - Hugo Muller
+		label1 = new JLabel();
+		panel1 = new JPanel();
+		button2 = new JButton();
+		button3 = new JButton();
+		panel2 = new JPanel();
+		label2 = new JLabel();
+		label3 = new JLabel();
+		label4 = new JLabel();
+		label5 = new JLabel();
+		label6 = new JLabel();
+		label7 = new JLabel();
+
+		//======== this ========
+		setBackground(new Color(102, 0, 102));
+
+		// JFormDesigner evaluation mark
+		setBorder(new javax.swing.border.CompoundBorder(
+			new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+				"JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+				javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+				java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+		setLayout(new GridBagLayout());
+		((GridBagLayout)getLayout()).columnWidths = new int[] {0, 164, 330, 152, 0, 0};
+		((GridBagLayout)getLayout()).rowHeights = new int[] {111, 259, 111, 0};
+		((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+		((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+
+		//---- label1 ----
+		label1.setText("Validator");
+		label1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		label1.setBackground(Color.black);
+		label1.setForeground(Color.white);
+		add(label1, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+			GridBagConstraints.CENTER, GridBagConstraints.NONE,
+			new Insets(0, 0, 5, 5), 0, 0));
+
+		//======== panel1 ========
+		{
+			panel1.setLayout(new GridBagLayout());
+			((GridBagLayout)panel1.getLayout()).columnWidths = new int[] {302, 63, 276, 0};
+			((GridBagLayout)panel1.getLayout()).rowHeights = new int[] {248, 0};
+			((GridBagLayout)panel1.getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+			((GridBagLayout)panel1.getLayout()).rowWeights = new double[] {0.0, 1.0E-4};
+
+			//---- button2 ----
+			button2.setText("   Visualize Order   ");
+			button2.setMaximumSize(new Dimension(160, 90));
+			button2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			button2.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					button2MouseClicked(e);
+				}
+			});
+			panel1.add(button2, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.NONE,
+				new Insets(0, 0, 0, 5), 100, 80));
+
+			//---- button3 ----
+			button3.setText("Create Scrapie Test");
+			button3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			button3.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					button3MouseClicked(e);
+				}
+			});
+			panel1.add(button3, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.NONE,
+				new Insets(0, 0, 0, 0), 100, 80));
+		}
+		add(panel1, new GridBagConstraints(1, 1, 3, 1, 0.0, 0.0,
+			GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+			new Insets(0, 0, 5, 5), 0, 0));
+
+		//======== panel2 ========
+		{
+			panel2.setLayout(new GridBagLayout());
+			((GridBagLayout)panel2.getLayout()).columnWidths = new int[] {145, 0};
+			((GridBagLayout)panel2.getLayout()).rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0};
+			((GridBagLayout)panel2.getLayout()).columnWeights = new double[] {0.0, 1.0E-4};
+			((GridBagLayout)panel2.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+
+			//---- label2 ----
+			label2.setText("Maxime Tanguy");
+			panel2.add(label2, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.NONE,
+				new Insets(0, 0, 5, 0), 0, 0));
+
+			//---- label3 ----
+			label3.setText("Cyprien Zouaoui");
+			panel2.add(label3, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.NONE,
+				new Insets(0, 0, 5, 0), 0, 0));
+
+			//---- label4 ----
+			label4.setText("Gael Beaurin");
+			panel2.add(label4, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.NONE,
+				new Insets(0, 0, 5, 0), 0, 0));
+
+			//---- label5 ----
+			label5.setText("Erwan Mazureau");
+			panel2.add(label5, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.NONE,
+				new Insets(0, 0, 5, 0), 0, 0));
+
+			//---- label6 ----
+			label6.setText("Hugo Muller");
+			panel2.add(label6, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.NONE,
+				new Insets(0, 0, 5, 0), 0, 0));
+
+			//---- label7 ----
+			label7.setText("Mathias Antunes");
+			panel2.add(label7, new GridBagConstraints(0, 5, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.NONE,
+				new Insets(0, 0, 0, 0), 0, 0));
+		}
+		add(panel2, new GridBagConstraints(3, 2, 1, 1, 0.0, 0.0,
+			GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+			new Insets(0, 0, 0, 5), 0, 0));
+		// JFormDesigner - End of component initialization  //GEN-END:initComponents
+	}
+
+	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+	// Generated using JFormDesigner Evaluation license - Hugo Muller
+	private JLabel label1;
+	private JPanel panel1;
+	private JButton button2;
+	private JButton button3;
+	private JPanel panel2;
+	private JLabel label2;
+	private JLabel label3;
+	private JLabel label4;
+	private JLabel label5;
+	private JLabel label6;
+	private JLabel label7;
+	// JFormDesigner - End of variables declaration  //GEN-END:variables
 }
